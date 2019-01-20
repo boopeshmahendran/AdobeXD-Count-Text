49541a35f2cdc7d67c40d25bc393e95f7b4c6711
Boopesh Mahendran <boopeshmahendran@Boopeshs-MacBook-Pro.local>
2019-01-20 20:44:28 +0530

    Handle error cases

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const { Text } = require("scenegraph");
-const { alert } = require("./lib/dialogs");
+const { alert, error } = require("./lib/dialogs");
 
 async function countText(selection) {
     let wc, cc;
 
     wc = cc = 0;
 
+    let textLayersSelected = false;
+
     selection.items.forEach(node => {
         if (node instanceof Text) {
+            textLayersSelected = true;
+
             wc += countWords(node.text);
             cc += node.text.length;
         }
     });
+
+    // Handle error cases
+    if (!textLayersSelected) {
+        await error("Count Text - Error", "No text layers selected");
+        return ;
+    }
 
     await alert(
         "Count Text",
