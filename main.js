4322aec20a406ca50f98d18e50575b54975cd8f8
Boopesh Mahendran <boopeshmahendran@Boopeshs-MacBook-Pro.local>
2019-01-20 22:13:59 +0530

    Add docs

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const { Text } = require("scenegraph");
 const { alert, error } = require("./lib/dialogs");
 
+/**
+ * Entry point for the plugin
+ *
+ * @param {!Selection} selection
+ */
 async function countText(selection) {
-    let wc = 0, cc = 0;
-
-    let textLayersSelected = false;
+    let wc = 0, cc = 0, textLayersSelected = false;
 
     selection.items.forEach(node => {
         if (node instanceof Text) {
@@ -26,6 +29,11 @@ async function countText(selection) {
     await alert("Count Text", `Word Count: ${wc}`, `Character Count: ${cc}`);
 }
 
+/**
+ * Counts the number of words in the string
+ * @param {!string} str
+ * @returns {Number} no of words in the string
+ */
 function countWords(str) {
     return str.trim().split(/\s+/).length;
 }
